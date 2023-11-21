fc29fdcfc7b32896fc7f5a8a134a77908b314cf0
elionavarroh <elionavarroh@gmail.com>
2023-11-21 23:58:45 +0100

    add the differents groups of music with their different info, also with emoticons and some style for the text

--- src/main.js
+++ src/main.js
@@ -1,44 +1,63 @@
 import "./style.css";
+
+//const para texto en negrita
+const Title = "font-size: 20px; font-weight: bold; background-color: green";
+
+//const para los géneros de música
+const PopRock = "🎵 Pop Rock";
+const Rock = "🎸 Rock";
+const HardRock = "🤘 Hard Rock";
+const Clasica = "🎼 Clásica";
+
 //Title of the bands
-console.log("Band's group");
+console.log("Band's group", Title);
 
 //Beatles
-console.log(
-".Name: Queen;"
+console.log("-Beatles")
+console.log({
+    name: "Beatles",
+    age: 1960,
+    active: "True",
+    genre: `${PopRock}`
+}
 );
 
 //Queen
+console.log("-Queen")
 console.log({
     name: "Queen",
-    age: "1970",
+    age: 1970,
     active: "False",
-    genre: "Rock"
+    genre: `${Rock}`
 }
 );
 
 //AC DC
+console.log("-AC DC")
 console.log({
     name: "AC DC",
-    age: "1973",
+    age: 1973,
     active: "True",
-    genre: "Hard Rock"
+    genre: `${HardRock}`
 }
 );
 
 //Ludwig Van Beethoven
+console.log("-Ludwig Van Beethoven")
 console.log({
     name: "Ludwig Van Beethoven",
-    age: "1770",
+    age: 1770,
     active: "False",
-    genre: "Clásica"
+    genre: `${Clasica}`
 }
 );
 
 //The Rolling Stones
+console.log("-The Rolling Stones")
 console.log({
     name: "The Rolling Stones",
-    age: "1962",
+    age: 1962,
     active: "True",
-    genre: "Rock"
+    genre: `${Rock}`
 }
 );
